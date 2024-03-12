aefcc102c84680e73b68d6f2acd8a7b7acc5614c
David Fleming <tonage72@gmail.com>
2024-03-12 18:35:23 -0400

    js working looks good on to success message

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,26 @@
-const emailInput = document.querySelector('#email');
-const subscribeButton = document.querySelector('.subscribe-button');
+const subscribeButtonInvalid = document.querySelector('.subscribe-button-invalid');
+const subscribeButtonValid = document.querySelector('.subscribe-button-valid');
+const mainDiv = document.querySelector('main');
+const successDiv = document.querySelector('.success-message');
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
+
+function validation() {
+
+	let email = document.getElementById('email').value;
+
+	if (email.match(pattern)) {
+		console.log("valid");
+		subscribeButtonInvalid.style.display = 'none';
+		subscribeButtonValid.style.display = 'block';
+	} else {
+		console.log("not");
+		subscribeButtonInvalid.style.display = 'block';
+		subscribeButtonValid.style.display = 'none';
+	}
+}
+
+function submitEmail() {
+	mainDiv.style.display = 'none';
+	successDiv.style.display = 'block';
+}
